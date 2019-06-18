fcfadf7b6edfe2276c59e0979509c048cde63c8a
NeutralDread <jordanamiles@hotmail.com>
2019-06-17 22:39:16 -0500

    Console logging the component tree to visualize

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,18 +3,21 @@ import Counter from './counter';
 
 class Counters extends Component {
   render() {
-    const { onReset, counters, onDelete, onIncrement } = this.props;
+    console.log('Counters - Rendered');
 
     return (
       <div>
-        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+        <button
+          onClick={this.props.onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
           Reset
         </button>
-        {counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={onDelete}
-            onIncrement={onIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           />
         ))}
